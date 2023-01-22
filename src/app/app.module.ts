32345b343dcca4e90d54c328f8d4eb9cefb2581e
John Pier <warhamer10tw@gmail.com>
2023-01-22 17:27:28 +0400

    upd to 9v

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainViewComponent } from './components/main-view/main-view.component';
